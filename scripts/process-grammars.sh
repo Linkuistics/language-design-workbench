96fccfd49bdb8d564a428eb2eaf7f10b923d1961
Antony Blakey <antony.blakey@gmail.com>
2024-10-11 05:48:31 +1100

    Refactor grammar processing to migrate outputs to next-src, update CLI scripts, and remove deprecated grammar artifacts

--- scripts/process-grammars.sh
+++ scripts/process-grammars.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env zsh
 
-for grammar in src/languages-meta/**/*.grammar ; do
-    echo "Processing $grammar => $grammar.json"
-    ts-node src/cli/ldw.ts grammar-to-json -i $grammar -o $grammar.json
-    echo "Processing $grammar => $grammar.model"
-    ts-node src/cli/ldw.ts grammar-to-model -i $grammar -o $grammar.model
+rm -rf next-src
+cp -r src next-src
+
+for grammar in next-src/languages-meta/**/*.grammar ; do
+
+    # echo "Processing $grammar => $grammar.json"
+    # ts-node src/cli/ldw.ts grammar-to-json -i $grammar -o $grammar.json
+
+    model=${grammar%.grammar}.model
+    echo "Processing $grammar => $model"
+    ts-node src/cli/ldw.ts grammar-to-model -i $grammar -o $model
+
     echo
+
 done
--- scripts/process-models.sh
+++ scripts/process-models.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env zsh
 
-for model in src/languages-meta/**/*.model ; do
-    echo "Processing $model => $model.ts"
-    ts-node src/cli/ldw.ts model-to-types -l typescript -i $model -o $model.ts
-    prettier -w $model.ts
+for model in next-src/languages-meta/**/*.model ; do
 
-    echo "Processing $model => $model.rs"
-    ts-node src/cli/ldw.ts model-to-types -l rust -i $model -o $model.rs
-    rustfmt $model.rs
+    dir=$(dirname $model)
 
-    echo "Processing $model => $model.visitor.ts"
-    ts-node src/cli/ldw.ts model-to-visitor -i $model -o $model.visitor.ts
-    prettier -w $model.visitor.ts
+    echo "Processing $model => $dir/model.ts"
+    ts-node src/cli/ldw.ts model-to-types -l typescript -i $model -o $dir/model.ts
+    prettier -w $dir/model.ts
+
+    echo "Processing $model => $dir/model.rs"
+    ts-node src/cli/ldw.ts model-to-types -l rust -i $model -o $dir/model.rs
+    rustfmt $dir/model.rs
+
+    echo "Processing $model => $dir/visitor.ts"
+    ts-node src/cli/ldw.ts model-to-visitor -i $model -o $dir/visitor.ts
+    prettier -w $dir/visitor.ts
+
     echo
+
 done
