5a60260d6d91803e480a406e5e3df6271d2effbd
Antony Blakey <antony.blakey@gmail.com>
2024-10-12 20:20:33 +1100

    Update .gitignore to exclude src/gen2 and remove obsolete grammar processing scripts. Add gen2 build/reset scripts

--- scripts/gen1-build.sh
+++ scripts/gen1-build.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env zsh
 
+for grammar in src/gen1/languages/ldw/**/*.grammar ; do
+
+    echo
+
+    # json=${grammar%.grammar}.model
+    # echo "Generating $json from $(basename $grammar)"
+    # ts-node src/gen0/cli/ldw.ts grammar-to-json -i $grammar -o $json
+    
+    model=${grammar%.grammar}.model
+    echo "Generating $model from $(basename $grammar)"
+    ts-node src/gen0/cli/ldw.ts grammar-to-model -i $grammar -o $model
+
+done
+
 for model in src/gen1/languages/ldw/**/*.model ; do
 
     dir=$(dirname $model)/$(basename $model .model)
---
+++ scripts/gen1-reset.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+echo "Copy gen0 to gen1"
+
+rm -rf src/gen1
+cp -r src/gen0 src/gen1
